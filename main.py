e272ba3b105131df90ffb82d469e84c0d31e108b
d <d@github.com>
2025-04-28 00:08:26 +0200

    auth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import functions_framework
+import google.auth.transport.requests
+import google.oauth2.id_token
 import requests
-from flask import make_response
+from flask import make_response, jsonify
 from google.cloud import secretmanager
 
 # Set variables
@@ -15,6 +17,27 @@ name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
 
 @functions_framework.http
 def fetch_and_store_data(request):
+    auth_header = request.headers.get('Authorization')
+    if not auth_header or not auth_header.startswith('Bearer '):
+        return jsonify({'error': 'Unauthorized: Missing Bearer token'}), 401
+    id_token = auth_header.split(' ')[1]
+    try:
+        # Validate the ID Token
+        request_adapter = google.auth.transport.requests.Request()
+        claims = google.oauth2.id_token.verify_firebase_token(id_token, request_adapter)
+
+        if not claims:
+            return jsonify({'error': 'Unauthorized: Invalid token'}), 401
+        # If valid
+        # return jsonify({
+        #     'message': 'Hello, secure world!',
+        #     'user': claims.get('email'),
+        #     'uid': claims.get('user_id')
+        # })
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Unauthorized: Token verification failed'}), 401
+
     origin = request.headers.get('Origin', '')
     cors_headers = {
         'Access-Control-Allow-Origin': ALLOWED_ORIGIN if origin == ALLOWED_ORIGIN else '',
